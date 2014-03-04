6cc337010edffe72e30af820bc7c7ed2e6a6cf77
cc14514 <cc14514@github.com>
2014-03-04 15:48:49 +0800

    fix bug

--- src/ecache_main.erl
+++ src/ecache_main.erl
@@ -1,8 +1,5 @@
 -module(ecache_main).
 
--include_lib("exmpp/include/exmpp.hrl").
--include_lib("exmpp/include/exmpp_client.hrl").
-
 -behaviour(gen_server).
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 
